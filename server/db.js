58dca97fd8968e841655f6c08850d47aff6fac76
Ivan Afanasev <diykarelia@gmail.com>
2023-04-27 13:19:50 +0200

    init mongo atlas

--- server/db.js
+++ server/db.js
@@ -1,11 +1,13 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-mongoose.connect(`${process.env.DB_URL}${process.env.DB_NAME}`)
+mongoose.connect(
+  'mongodb+srv://Cluster63119:cUZ4b1tEZ31M@cluster63119.tvwb1rp.mongodb.net/LegacyProject?retryWrites=true&w=majority'
+);
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
 
-module.exports = mongoose
+module.exports = mongoose;
