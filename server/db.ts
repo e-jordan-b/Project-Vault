121a3505c4864078061c72ecd97fb1ef383f63d6
Paola <paola.patino.castillo@gmail.com>
2023-04-27 15:54:18 +0200

    Implement TypeScript on DB, Index and router files

--- server/db.ts
+++ server/db.ts
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 
-mongoose.connect(`${process.env.DB_URL}${process.env.DB_NAME}`)
+mongoose.connect(`${process.env.ATLAS_URL}`);
 
 const db = mongoose.connection
 
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
-module.exports = mongoose
+export default mongoose
