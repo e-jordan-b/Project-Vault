65540dd045c2a3b0be3f147d572f8579b7176215
Ivan Afanasev <diykarelia@gmail.com>
2023-04-27 14:19:24 +0200

    fix: .env

--- server/db.js
+++ server/db.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  'mongodb+srv://Cluster63119:cUZ4b1tEZ31M@cluster63119.tvwb1rp.mongodb.net/LegacyProject?retryWrites=true&w=majority'
-);
+mongoose.connect(`${process.env.ATLAS_URL}`);
 
 const db = mongoose.connection;
 
