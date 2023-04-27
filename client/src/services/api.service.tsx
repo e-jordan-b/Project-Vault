2474bd2847a18dc17976ddf5c3afaeb0482df577
Michael Epelboim <mepelboim@outlook.com>
2023-04-27 15:58:44 +0200

    created tsconfig.json file

--- client/src/services/api.service.tsx
+++ client/src/services/api.service.tsx
@@ -1,1 +1,8 @@
 import hhtp from '../http-common';
+import User from '../types/user.type';
+import Project from '../types/project.type';
+class ApiService {
+  updateProject(data: Project, id: string) {
+    return hhtp.put<any>(`/projects/${data.id}`, data);
+  }
+}
