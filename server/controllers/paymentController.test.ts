b3c50245761d87da8b0f86262e9947370fc2863f
Paola <paola.patino.castillo@gmail.com>
2023-04-28 18:21:06 +0200

    TEST: paymentController

--- server/controllers/paymentController.test.ts
+++ server/controllers/paymentController.test.ts
@@ -1,7 +1,4 @@
-import app from '../app';
-import router from '../routes/router';
 import { describe, test } from '@jest/globals';
-import request from 'supertest';
 import Stripe from 'stripe';
 import dotevn from 'dotenv';
 dotevn.config()
@@ -12,13 +9,30 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 describe('Stripe Payment Intents:', () => {
-  app.use(router);
+  let paymentIntentId: string;
 
-  test.only('Stripe test are happening', () => {
-    console.log('Yes!')
-  })
+  beforeAll(async () => {
+    // Create a new Payment Intent
+    var paymentIntent = await stripe.paymentIntents.create({
+      amount: 1000,
+      currency: 'eur',
+      payment_method_types: ['card'],
+    });
+    paymentIntentId = paymentIntent.id;
+  });
 
-  // test('Route /projects endpoint should return a 200: ', async () => {
-  //   await request(app).get('/projects').expect(200).expect('Content-Type', /json/)
-  // })
+  afterAll(async () => {
+    // Cancel the Payment Intent after the tests are done
+    await stripe.paymentIntents.cancel(paymentIntentId);
+  });
+
+  it('creates a new Payment Intent', async () => {
+    // Retrieve the Payment Intent from the Stripe API
+    const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
+    expect(paymentIntent.amount).toBe(1000);
+    expect(paymentIntent.currency).toBe('eur');
+    expect(paymentIntent.payment_method_types).toEqual(['card']);
+  });
+
+
 })
