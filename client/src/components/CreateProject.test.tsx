98d707607d349923f892ab6e1c6b0cfab4b256e7
Paola <paola.patino.castillo@gmail.com>
2023-04-29 17:19:35 +0200

    test-1 for createPost

---
+++ client/src/components/CreateProject.test.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import CreateProject from './CreateProject';
+
+describe('CreateProject', () => {
+  it('submits the form', async () => {
+    const mockNavigate = jest.fn();
+    const mockCreateProject = jest.fn().mockResolvedValueOnce({ status: 200 });
+    const onClose = jest.fn();
+
+    const { getByLabelText, getByText } = render(
+      <CreateProject open={true} onClose={onClose} />,
+      {
+        // Mocking the useNavigate hook
+        wrapper: ({ children }) => (
+          <div>{children}</div>
+        ),
+      }
+    );
+
+    const titleInput = getByLabelText('Title:');
+    fireEvent.change(titleInput, { target: { value: 'Test Project' } });
+
+    const descriptionInput = screen.getByRole('textbox', { name: 'Rich Text Editor, snow' });
+    fireEvent.change(descriptionInput, { target: { value: 'Test description' } });
+
+    const imageInput = getByLabelText('Image:');
+    Object.defineProperty(imageInput, 'files', {
+      value: [
+        new File(['test'], 'test.png', { type: 'image/png' }),
+      ],
+    });
+    fireEvent.change(imageInput);
+
+    const tagsInput = getByLabelText('Tags:');
+    fireEvent.change(tagsInput, { target: { value: 'test, project' } });
+
+    const submitButton = getByText('Create new project');
+    fireEvent.click(submitButton);
+
+    await screen.findByText('Your new project');
+
+    expect(mockCreateProject).toHaveBeenCalledWith({
+      title: 'Test Project',
+      description: 'Test description',
+      tags: 'test, project',
+      quillValue: 'Test description',
+      image: '',
+      createdBy: null,
+      updates: [],
+      chat: [],
+      author: '',
+      date: '',
+    });
+
+    expect(mockNavigate).toHaveBeenCalledWith('/posts/null');
+    expect(onClose).toHaveBeenCalled();
+  });
+});
