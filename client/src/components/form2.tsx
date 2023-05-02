86b39622b6228c05548f0355458bf71254b4e200
Paola <paola.patino.castillo@gmail.com>
2023-05-02 11:12:02 +0200

    Typescript done rendering tests done

--- client/src/components/form2.tsx
+++ client/src/components/form2.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { loadStripe } from '@stripe/stripe-js'
 import { Elements } from '@stripe/react-stripe-js'
-import Pay2 from './pay2'
+import Pay from './Pay'
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY as string)
 
 function Form2 () {
   return (
     <Elements stripe={stripePromise}>
-      <Pay2/>
+      <Pay/>
     </Elements>
   )
 }
