0ca1a3e41bc2ad95bd920af362d6f6b5066de6b3
Paola <paola.patino.castillo@gmail.com>
2023-05-03 16:37:51 +0200

    e2e-test-donations-done

--- client/src/components/Pay.tsx
+++ client/src/components/Pay.tsx
@@ -70,13 +70,13 @@ function Pay() {
           <div className='showMessage' style={{ zIndex: 2 }}>
             <button
               onClick={onClose}
-              className='closeButton'
+              className='closePayButton'
               role='close-button'
             >
               X
             </button>
             {showMessage === 'error'
-              ? <h1>There's been a problem with your donation, <br></br> please try an alternative card.</h1>
+              ? <h1>There&apos;s been a problem with your donation, <br></br> please try an alternative card.</h1>
               : <h1>Your donation has been made, <br></br> Thank you for your support! ✨</h1>
             }
           </div>
@@ -88,7 +88,7 @@ function Pay() {
       >
         <button
           onClick={onClose}
-          className='closeButton'
+          className='closePayButton'
           role='close-button'
         >
           X
@@ -124,71 +124,3 @@ function Pay() {
 }
 
 export default Pay;
-
-// import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
-// import axios from 'axios'
-// import { useNavigate } from 'react-router-dom'
-// import '../styles/donationForm.css'
-
-// const serverURL = process.env.REACT_APP_SERVER
-
-// function Pay2 () {
-//   const [amount, setAmount] = useState('')
-//   const [isProcessing, setIsProcessing] = useState(false)
-//   const stripe = useStripe()
-//   const elements = useElements()
-//   const navigate = useNavigate()
-
-//   function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
-//     setAmount(e.target.value)
-//   }
-
-//   async function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
-//     e.preventDefault()
-//     const { error, paymentMethod } = await stripe?.createPaymentMethod({
-//       type: 'card',
-//       card: elements.getElement(CardElement)
-//     })
-
-//     setIsProcessing(true)
-
-//     if (!error) {
-//       try {
-//         const { id } = paymentMethod
-//         const wholeAmount = Number(amount) * 100
-//         const response = await axios.post(`${serverURL}/create-payment-intent`, {
-//           wholeAmount,
-//           id
-//         })
-//         if (response.data.succes) {
-//           console.log('Succesfull payment')
-//           navigate('/home')
-//         }
-//       } catch (error) {
-//         console.log(error)
-//       }
-//     } else {
-//       console.log(error.message)
-//     }
-//   }
-//   return (
-//     <div className='donationDiv'>
-//       <form onSubmit={handleSubmit} className='donationForm'>
-//         <label htmlFor='amount'>Amount:</label>
-//         <input
-//         type='number'
-//         id='amount'
-//         name='amount'
-//         value={amount}
-//         className='donationAmount'
-//         onChange={handleChange}></input>
-//         <div>
-//           <CardElement/>
-//         </div>
-//         <button className='donateButton'>{isProcessing ? 'Processing...' : 'Donate'}</button>
-//       </form>
-//     </div>
-//   )
-// }
-
-// export default Pay2
